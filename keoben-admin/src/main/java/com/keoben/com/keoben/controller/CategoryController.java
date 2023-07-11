ce0e77e931ffc85347ef27811f4b2eadc699e908
KeoBen <1207257586@qq.com>
2023-07-11 15:41:54 +0800

    Finish 5.9.分类Excel导出

--- keoben-admin/src/main/java/com/keoben/com/keoben/controller/CategoryController.java
+++ keoben-admin/src/main/java/com/keoben/com/keoben/controller/CategoryController.java
@@ -1,11 +1,17 @@
 package com.keoben.com.keoben.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.keoben.domain.ResponseResult;
 import com.keoben.domain.entity.Category;
+import com.keoben.domain.enums.AppHttpCodeEnum;
 import com.keoben.domain.vo.CategoryVo;
+import com.keoben.domain.vo.ExcelCategoryVo;
 import com.keoben.domain.vo.TagVo;
 import com.keoben.service.CategoryService;
 import com.keoben.service.TagService;
+import com.keoben.utils.BeanCopyUtils;
+import com.keoben.utils.WebUtils;
 import javafx.stage.StageStyle;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RestController
@@ -30,6 +38,24 @@ public class CategoryController {
 		return ResponseResult.okResult(list);
 	}
 
+	@GetMapping("/export")
+	public void export(HttpServletResponse response){
+		try {
+			//设置下载文件的请求头
+			WebUtils.setDownLoadHeader("分类.xlsx",response);
+			//获取需要导出的数据
+			List<Category> categoryVos = categoryService.list();
 
+			List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+			//把数据写入到Excel中
+			EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出")
+					.doWrite(excelCategoryVos);
+
+		} catch (Exception e) {
+			//如果出现异常也要响应json
+			ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+			WebUtils.renderString(response, JSON.toJSONString(result));
+		}
+	}
 
 }
